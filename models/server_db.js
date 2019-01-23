272b1e75dd79bf03970c71d85adddd22c9ddebc1
OmprakashDhakadgit config --global user.name OmprakashDhakad <opdhakad25@gmail.com>
2019-01-23 19:28:11 +0530

    Deleting all users

--- models/server_db.js
+++ models/server_db.js
@@ -30,7 +30,16 @@ get_list_of_participants = (data) => new Promise((resolve,reject) => {
     })
 })
 
+delete_users = (data) => new Promise((resolve,reject)=>{
+db.query('delete from lottery_information where ID > 0',null,function(err,results,fields){
+    if(err){
+        reject('could not delete all users');
+    }
+    resolve("successfull deleted all users");
+})
+})
 module.exports = {
     save_user_information,
-    get_list_of_participants
+    get_list_of_participants,
+    delete_users
 }
